37b270d77704243d96196f3579ec9bc7fd6650cd
jpratt <jpratt@mtroyal.ca>
2020-08-14 09:04:55 -0600

    Remove unneeded imports.

--- src/main/java/cisummarizer/JunitProblemCollector.java
+++ src/main/java/cisummarizer/JunitProblemCollector.java
@@ -3,7 +3,6 @@ package cisummarizer;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
